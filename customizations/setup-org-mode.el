09cc7ebed1b8911a2843e4e95e63a8d33c66e93d
Caleb Gregory <calebgregory@gmail.com>
2022-09-13 09:18:11 -0600

    misc: adds clojure

--- customizations/setup-org-mode.el
+++ customizations/setup-org-mode.el
@@ -2,4 +2,5 @@
 ;; [full set of languages](https://orgmode.org/worg/org-contrib/babel/languages/index.html)
 (org-babel-do-load-languages
  'org-babel-load-languages
- '((python . t)))
+ '((python . t)
+   (clojure . t)))
